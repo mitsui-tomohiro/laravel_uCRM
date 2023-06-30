59e27831c80505e22a5f9942871c289e3e93c844
Mitsui Tomohiro <tomohiro.mitsui@adglobe.co.jp>
2023-06-30 11:43:41 +0900

    inertia-vue.js

---
+++ resources/js/Pages/ComponentTest.vue
@@ -0,0 +1,13 @@
+<script setup>
+import InputLabel from '@/Components/InputLabel.vue'
+import TextInput from '@/Components/TextInput.vue'
+import GuestLayout from '@/Layouts/GuestLayout.vue';
+
+</script>
+
+<template>
+    <GuestLayout>
+        <InputLabel>タイトル</InputLabel>
+        <TextInput modelValue="初期値です"></TextInput>
+    </GuestLayout>
+</template>
