0ae7f72830110eb9b2ae8ed743fe051e0769b45c
Mitsui Tomohiro <tomohiro.mitsui@adglobe.co.jp>
2023-07-05 09:39:02 +0900

    customers

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\CustomerController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
@@ -8,7 +9,9 @@ use App\Http\Controllers\InertiaTestController;
 use App\Http\Controllers\ItemController;
 
 Route::resource('items', ItemController::class)
-    ->middleware((['auth', 'verified']));
+->middleware((['auth', 'verified']));
+Route::resource('customers', CustomerController::class)
+->middleware((['auth', 'verified']));
 
 Route::get('/inertia-test', function () {
     return Inertia::render('InertiaTest');
